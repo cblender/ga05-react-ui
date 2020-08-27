2b3489c2fb4d6cd565006a4af2e98c3233151309
cblender <christopher.blender@gmail.com>
2020-08-27 15:31:19 -0400

    implemented API calling.  main array.push has issues

--- pokemonreact/src/App.js
+++ pokemonreact/src/App.js
@@ -1,18 +1,56 @@
+/*$$$$$$$           /$$                /$$ /$$$$$$$                                  /$$    
+| $$__  $$         | $$               | $/| $$__  $$                                | $$    
+| $$  \ $$ /$$$$$$ | $$   /$$  /$$$$$$|_/ | $$  \ $$  /$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$  
+| $$$$$$$//$$__  $$| $$  /$$/ /$$__  $$   | $$$$$$$/ /$$__  $$ |____  $$ /$$_____/|_  $$_/  
+| $$____/| $$  \ $$| $$$$$$/ | $$$$$$$$   | $$__  $$| $$$$$$$$  /$$$$$$$| $$        | $$    
+| $$     | $$  | $$| $$_  $$ | $$_____/   | $$  \ $$| $$_____/ /$$__  $$| $$        | $$ /$$
+| $$     |  $$$$$$/| $$ \  $$|  $$$$$$$   | $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$  |  $$$$/
+|__/      \______/ |__/  \__/ \_______/   |__/  |__/ \_______/ \_______/ \_______/   \___*/
+                                                                                            
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
-    const pokeSpecies = 151;
-    const pokeGeneration = 1;
+  }
 
+  pokeQuantity = 151
+  pokeGeneration = 1
+  pokeList = []
+
+  fetchPokeList () {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then(response => response.json())
+    .then(function(allpokemon){
+      allpokemon.results.forEach(function(pokemon){
+        let url = pokemon.url
+        fetch(url)
+        .then(response => response.json())
+        .then(function(pokeData) {
+          this.pokeList.push(pokeData)
+          console.log(pokeData)
+        })
+      })
+    })
+    console.log(this.pokeList)
+  }
+
+  fetchPokemon (pokemon) {
+    let url = pokemon.url
+    fetch(url)
+    .then(response => response.json())
+    .then(function(pokeData) {
+      console.log(pokeData)
+    })
   }
 
   render () {
+
+    this.fetchPokeList();
+
     return (
-      <div class="appContainerDoNotTouch">
+      <div className="appContainerDoNotTouch">
         <div>
           <h1>PokeReact</h1>
         </div>
